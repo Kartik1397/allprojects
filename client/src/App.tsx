832be105574553e05121f893e8f09d953de830e1
Kartik <kartikprajapati987@gmail.com>
2021-12-26 17:24:17 +0530

    Test fontend backend connection

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,12 +1,23 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const [state, setState] = useState('');
+
+    useEffect(() => {
+        (async () => {
+            const r = await fetch('https://api.allprojects.ml');
+            const text = await r.text();
+            setState(text);
+        })();
+    }, []);
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        { state }
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
