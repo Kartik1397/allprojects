d3de2f76d485c4db2b8d922d9e8f9463c119ca46
Kartik <kartikprajapati987@gmail.com>
2021-12-26 17:39:51 +0530

    Test db connection

--- server/app.ts
+++ server/app.ts
@@ -1,16 +1,25 @@
 import express from 'express';
 import cors from 'cors';
 import * as http from 'http';
+import { MongoClient } from 'mongodb';
+
+const url = 'mongodb+srv://deep:deep2000@cluster0.habkm.mongodb.net';
+const client = new MongoClient(url);
 
 const app: express.Application = express();
 const server: http.Server = http.createServer(app);
-const port = 3001;
+const port = process.env.PORT || 80;
 
 app.use(express.json());
 app.use(cors());
 
-app.get('/', (req: express.Request, res: express.Response) => {
-    res.status(200).send('running');
+app.get('/', async (req: express.Request, res: express.Response) => {
+    await client.connect();
+    const db = client.db('allprojects');
+    const collection = db.collection('users');
+    const users = await collection.find({}).toArray();
+    res.status(200).send(JSON.stringify(users));
+    client.close();
 });
 
 server.listen(port, () => {
