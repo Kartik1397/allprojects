a78d82f8a78cf21eead336ab38d3bb09027bd484
deepshah1309 <deepshah1309@gmail.com>
2021-12-26 20:10:19 +0530

    Mongoose Connection, Middleware, Skeleton (Model,Route)

--- server/app.ts
+++ server/app.ts
@@ -1,26 +1,55 @@
 import express from 'express';
 import cors from 'cors';
 import * as http from 'http';
-import { MongoClient } from 'mongodb';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
 
-const url = 'mongodb+srv://deep:deep2000@cluster0.habkm.mongodb.net';
-const client = new MongoClient(url);
+//route Imports
+const userRoutes = require('./Routes/UserRoutes');
 
 const app: express.Application = express();
 const server: http.Server = http.createServer(app);
+declare var process : {
+    env: {
+      MONGO_URL: string,
+      PORT:number,
+    }
+  }
 const port = process.env.PORT || 80;
+
+
+
+//Config for Environment variables
+dotenv.config();
+
+
+//Mongoose Configurations
+mongoose.set("debug",true);
+// mongoose.set("useNewUrlParser", true);
+// mongoose.set("useFindAndModify", false);
+// mongoose.set("useCreateIndex", true);
 
 app.use(express.json());
 app.use(cors());
 
-app.get('/', async (req: express.Request, res: express.Response) => {
-    await client.connect();
-    const db = client.db('allprojects');
-    const collection = db.collection('users');
-    const users = await collection.find({}).toArray();
-    res.status(200).send('Test auto deploy ' + JSON.stringify(users));
-    client.close();
-});
+mongoose.connect(
+    process.env.MONGO_URL,
+    () => {
+      console.log(process.env.MONGO_URL);
+      console.log("Connected to MongoDB");
+    }
+  );
+
+
+
+  //cross origin resource sharing :Set middleware
+app.use(cors({origin: "https://allprojects.ml"}))
+
+app.use('/user',userRoutes);
+
+app.get('/',(req: express.Request, res: express.Response)=>{
+    res.status(200).send("server is under development ...");
+})
 
 server.listen(port, () => {
     console.log('Server started at ' + port);
