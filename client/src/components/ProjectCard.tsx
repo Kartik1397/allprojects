ed683c2ebc219bfe3dd806f01b07e2880a64634d
deep shah <deepshah1309@gmail.com>
2022-01-08 23:07:33 +0530

    stash

--- client/src/components/ProjectCard.tsx
+++ client/src/components/ProjectCard.tsx
@@ -3,9 +3,14 @@ import style from "./ProjectCard.module.css";
 
 const ProjectCard:FC = () => {
     return (
-        <div className={style.ProjectCard}>
-                        
-        </div>
+        <div className="card" style={{width: "18rem"}}>
+          <img className="card-img-top" src="./" alt="cap"/>
+          <div className="card-body">
+          <h5 className="card-title">Card title</h5>
+          <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+          <a href="#CV" className="btn btn-primary">Go somewhere</a>
+         </div>
+      </div>
     );
 }
 
